1bb767240f557f8096f995e9d8a6c1df2065d2b5
André Linhares Dev <de.lin@hotmail.com>
2024-01-10 21:49:22 -0300

    insert h1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 
 import './App.css';
+import imageCoffee from './assets/img/coffee-mug.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={imageCoffee} className="App-logo" alt="logo" />
+        <h1>An online Coffee store project</h1>
       </header>
     </div>
   );
