a82ad894232a598b4470f8b038a4c29ead270f81
André Linhares Dev <de.lin@hotmail.com>
2024-03-10 19:08:49 -0300

    add checkout button function

--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -11,13 +11,14 @@ const CartWidget = () => {
             <NavLink
                 to={"/shopping-cart"}
                 className={({ isActive }) => (isActive ? "text-amber-200" : "")}
+            
                 >
-
-            </NavLink>
                 <button className='flex items-center justify-center p-2 bg-transparent mx-4 relative'>
                     <ShoppingBagIcon className="h-7 w-7 text-amber-100" />
                     {shoppingCartItems.length >= 1 && <span className='bg-red-700 w-4 h-4 rounded-2xl absolute top-0 left-0 text-white font-medium text-xs flex items-center justify-center'>{shoppingCartItems.length}</span>}
                 </button>
+
+            </NavLink>
         </div>
     )
 }
--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -1,12 +1,14 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ShoppingCartItem } from "../ShoppingCartItem";
 import AppContext from "../../context/AppContext";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 
 
 
 function ShoppingCart() {
 
-    const { shoppingCartItems } = useContext(AppContext);
+    const { shoppingCartItems, setShoppingCartItems } = useContext(AppContext);
+    const [purchaseCompleted, setPurchaseCompleted] = useState(false);
 
     // Função para calcular o preço total do carrinho
     const calculateTotalPrice = () => {
@@ -18,29 +20,40 @@ function ShoppingCart() {
         return totalPrice;
     };
 
+    const handleCheckout = () => {
+        // Limpar o carrinho e marcar a compra como concluída
+        setShoppingCartItems([]);
+        setPurchaseCompleted(true);
+    };
+
     return (
         <section className="pt-[220px]">
             <h1 className="flex justify-center mb-6 font-bold text-5xl font-headers text-teal-950">Shopping Cart Resume</h1>
-            <div className="mx-[18vw] mb-10 flex flex-col rounded-3xl bg-teal-950 justify-center">
-                <>
-                    {shoppingCartItems.map((shoppingCartItem) => <ShoppingCartItem key={shoppingCartItem.id} quantity={shoppingCartItem.quantity} product={shoppingCartItem.product} />)}
-                </>
-                <>
-                    <h3 className="text-center text-white font-bold my-3">
-                        Total: ${calculateTotalPrice()}
-                    </h3>
-                    {shoppingCartItems.length > 0 && (
-  <button
-    type="button"
-    className="button_remove-item h-10 bg-teal-800 text-amber-200 font-semibold rounded-md p-2 hover:bg-teal-900"
-  >
-    Checkout
-  </button>
-)}
-                </>
+            <div className="mx-[20vw] mb-10 flex flex-col rounded-3xl bg-teal-950 justify-center">
+                {shoppingCartItems.map((shoppingCartItem) => (
+                    <ShoppingCartItem key={shoppingCartItem.id} quantity={shoppingCartItem.quantity} product={shoppingCartItem.product} />
+                ))}
+                <h3 className="text-center text-white font-bold my-3">
+                    Total: ${calculateTotalPrice()}
+                </h3>
+                {shoppingCartItems.length > 0 && !purchaseCompleted && (
+                    <button
+                        type="button"
+                        className="button_remove-item h-10 bg-teal-800 text-amber-200 font-semibold rounded-md p-2 hover:bg-teal-900"
+                        onClick={handleCheckout}
+                    >
+                        Checkout
+                    </button>
+                )}
+                {purchaseCompleted && (
+                    <div className="flex flex-row align-center justify-center">
+                        <p className="text-center text-lg text-green-500 font-bold my-3">Compra finalizada!</p>
+                        <CheckCircleIcon className="ml-1 w-7 flex justify-center text-green-400 animate-pulse" />
+                    </div>
+                )}
             </div>
         </section>
-    )
+    );
 }
 
 export { ShoppingCart };
